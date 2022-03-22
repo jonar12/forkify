34fcc9a8ff257f0f81aacb4d661ef97b773198b5
jonar12 <jona.arredondo12@gmail.com>
2022-03-21 23:32:58 -0400

    added error handling

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -5,6 +5,8 @@ import { Fraction } from 'fractional';
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
+  #errorMessage = 'We could not find that recipe. Please try another one!';
+  #message = '';
 
   render(data) {
     this.#data = data;
@@ -128,7 +130,36 @@ class RecipeView {
     </svg>
     </div>
     `;
-    this.#parentElement.innerHTML = '';
+    this.#clear();
+  }
+
+  renderError(message = this.#errorMessage) {
+    const markup = `
+      <div class="error">
+      <div>
+        <svg>
+          <use href="${icons}#icon-alert-triangle"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+      </div>
+    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderSuccess(message = this.#message) {
+    const markup = `
+      <div class="message">
+      <div>
+        <svg>
+          <use href="${icons}#icon-smile"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+      </div>
+    `;
+    this.#clear();
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
